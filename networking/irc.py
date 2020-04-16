e78b4ecaeb551d62b1a25a7eaeaede3ccb668933
Yutaro Mori <yuta_mori@outlook.jp>
2020-04-16 23:47:31 +0900

    networking part11 commit

--- networking/irc.py
+++ networking/irc.py
@@ -8,18 +8,18 @@ class IRC:
         self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def send(self, chan, msg):
-        self.irc.send("PRIVMSG " + chan + " " + msg + "\n")
+        self.irc._send_raw("PRIVMSG " + chan + " " + msg + "\n")
 
     def _send_raw(self, line, encoding="utf-8"):
-        self.irc.send(line.encode(encoding) + b"\r\n")
+        self.irc._send_raw(line.encode(encoding) + b"\r\n")
 
-        def connect(self, server, channel, botnick):
-            # defines the socket
-            print("connecting to:" + server)
-            self.irc.connect((server, 6667))     # connects to the server
-            self.irc.send("USER " + botnick + " " + botnick + " " + botnick + " :This is a fun bot!\n")   # user authentication
-            self.irc.send("NICK " + botnick + "\n")
-            self.irc.send("JOIN " + channel + "\n")  # join the channel
+    def connect(self, server, channel, botnick):
+        # defines the socket
+        print("connecting to:" + server)
+        self.irc.connect((server, 6667))     # connects to the server
+        self.irc._send_raw("USER " + botnick + " " + botnick + " " + botnick + " :This is a fun bot!\n")   # user authentication
+        self.irc._send_raw("NICK " + botnick + "\n")
+        self.irc._send_raw("JOIN " + channel + "\n")  # join the channel
 
     def get_text(self):
         text=self.irc.recv(2040)    # receive the text
